34260abf64636364aa38aa06a4ab8f427771882e
e <954053260@qq.com>
2017-07-10 15:56:26 +0800

    tt

--- src/view/chat.vue
+++ src/view/chat.vue
@@ -2,8 +2,8 @@
   <div id="chat" class="container bc-page">
     <div class="chat" ref="chatList">
       <vue-pull-refresh :on-refresh="onRefresh">
-        <ul class="chat-list">
-          <li v-for="item in chats" class="chat-item">
+        <div class="chat-list">
+          <div v-for="item in chats" class="chat-item">
             <div v-if="item.fromUserNo == friendUserNo" class="row">
               <img :src="host + item.fromAvatar">
               <div class="col">
@@ -18,11 +18,11 @@
               </div>
               <img :src="host + item.fromAvatar">
             </div>
-          </li>
-        </ul>
+          </div>
+        </div>
       </vue-pull-refresh>
     </div>
-    <form class="chat-input">
+    <form class="chat-input" target="form-submit">
       <label>
         <input type="text" name="test" style="display:none"/>
         <input type="text" ref="input" @focus="scrollBottom" @keydown="sendMsg($event)" v-model="msg">
@@ -33,49 +33,31 @@
 <script type="text/ecmascript-6">
   import vuePullRefresh from 'vue-pull-refresh';
   export default {
-    name:'app',
+    name: 'chat',
     created: function () {
+      this.friendUserNo = this.$route.query.friendUserNo;
+      this.isLoading = true;
       this.$loading.show('获取消息列表...');
-      this.getMessages(() => {
+      this.$http.get('/user/chat/message/recent/list', {data: {
+        friendUserNo: this.friendUserNo,
+        token: this.token,
+        count: 10
+      }}).then((data) => {
+        this.isLoading = false;
         this.$loading.hide();
-      }, () => {
+        if (data.code == 0) {
+          this.chats = data.datas.messages;
+        } else {
+          this.$toast.info('获取消息列表失败');
+        }
+      }, function () {
+        this.isLoading = false;
         this.$loading.hide();
+        this.$toast.info('获取消息列表失败');
       });
     },
     mounted: function () {
-      var isLoad = false;
-
-      this.interval = setInterval(() => {
-        if (isLoad) return;
-        isLoad = true;
-        this.$http.get('/user/chat/message/list', {data: {
-          friendUserNo: this.$route.query.friendUserNo,
-          token: this.token,
-          pageNumber: 1,
-          pageSize: 20
-        }}).then((data) => {
-          if (data.code == 0) {
-             var last = this.chats[this.chats.length - 1];
-            data.datas.page.content.forEach((item) => {
-              if (!last || new Date(item.createTime) > new Date(last.createTime)) {
-                this.chats.push(item);
-              }
-            });
-
-            this.chats.sort(function (a, b) {
-              return new Date(a.createTime) -  new Date(b.createTime);
-            });
-
-          } else {
-            this.$toast.info('获取消息列表失败');
-          }
-
-          isLoad = false;
-        }, function () {
-          isLoad = false;
-          this.$toast.info('获取消息列表失败');
-        });
-      }, 1000);
+      this.interval = setInterval(this.getMessages, 1000);
     },
     destroyed: function () {
       clearInterval(this.interval);
@@ -83,10 +65,10 @@
     components: {vuePullRefresh},
     data: function () {
       return {
+        isLoading: false,
         msg: '',
-        pageNumber: 1,
-        hasData: true,
-        chats: []
+        chats: [],
+        friendUserNo: ''
       }
     },
     computed: {
@@ -101,69 +83,80 @@
       }
     },
     methods: {
-      getMessages: function (success, fail) {
-        if (!this.hasData) {
-          setTimeout(() => {
-            success();
-          },1000);
+      getMessages: function () {
+        var startTime;
+
+        if (this.isLoading) {
           return;
         }
 
-        this.$http.get('/user/chat/message/list', {data: {
-          friendUserNo: this.$route.query.friendUserNo,
+        startTime = Number(this.chats[this.chats.length - 1].createTime) + 1;
+        this.isLoading = true;
+        this.$http.get('/user/chat/message/recent/list', {data: {
+          friendUserNo: this.friendUserNo,
           token: this.token,
-          pageNumber: this.pageNumber,
-          pageSize: 10
+          count: 10,
+          startTime: startTime
         }}).then((data) => {
+          this.isLoading = false;
           if (data.code == 0) {
-            success();
-            var last = this.chats[this.chats.length - 1];
-            data.datas.page.content.forEach((item) => {
-              if (!last || new Date(item.createTime) > new Date(last.createTime)) {
-                this.chats.push(item);
-              }
-            });
-
-            this.chats.sort(function (a, b) {
-              return new Date(a.createTime) -  new Date(b.createTime);
-            });
-
-            if (data.datas.page.content.length > 10) {
-              this.hasData = false;
+            this.chats = this.chats.concat(data.datas.messages);
+            if (data.datas.messages.length) {
+              this.scrollBottom();
             }
-
           } else {
-            fail();
             this.$toast.info('获取消息列表失败');
           }
         }, function () {
+          this.isLoading = false;
           this.$toast.info('获取消息列表失败');
-          fail();
         });
       },
       onRefresh: function () {
+
+        if (this.$refs.chatList.scrollTop != 0) {
+          return;
+        }
+
         return new Promise((resolve, reject) => {
-          this.getMessages(resolve, reject);
+          var endTime =  Number(this.chats[0].createTime) - 1;
+          this.$http.get('/user/chat/message/history/list', {data: {
+            friendUserNo: this.friendUserNo,
+            token: this.token,
+            count: 10,
+            endTime: endTime
+          }}).then((data) => {
+            if (data.code == 0) {
+              resolve();
+              this.chats = data.datas.messages.concat(this.chats);
+            } else {
+              reject();
+              this.$toast.info('获取消息列表失败');
+            }
+          }, function () {
+            reject();
+            this.$toast.info('获取消息列表失败');
+          });
         });
       },
       sendMsg: function (e) {
-        if (e.keyCode == 13 && this.msg) {
-          this.$loading.show('发送消息');
+        var msg = this.msg;
+        if (e.keyCode == 13 && msg) {
+          this.msg = '';
+          this.$refs.input.blur();
           this.$http.post('/user/chat/message/send', {data: {
             token: this.token,
             toUserNo: this.$route.query.friendUserNo,
             msgType: 1,
-            content: this.msg
+            content: msg
           }}).then((data) => {
-            this.$loading.hide();
             if (data.code == 0) {
-              this.msg = '';
-              this.scrollBottom();
+              this.getMessages();
+              this.$refs.input.blur();
             } else {
               this.$toast.info('发送失败');
             }
           }, () => {
-            this.$loading.hide();
             this.$toast.info('发送失败');
           });
         }
